fe4b339513a841b3c47557e0f353d7f913adb01c
skhan305 <usajjad028@gmail.com>
2024-06-14 10:34:08 +0500

    add final file

---
+++ script.js
@@ -0,0 +1,73 @@
+   // Even Odd number Program
+
+
+                // let number = prompt("Enter your value")
+                // if( number % 2 === 0) {
+                //     alert( number + " is Even  Number");
+                // }
+                // else{
+                //     alert( number + " is Odd  Number");
+                // }
+
+
+                // Checking Eligibilty program
+
+
+                // alert ( "First your marks is check for this course");
+                //  let totalMarks= +prompt("enter your marks");
+                //  if ( totalMarks >= 50 && totalMarks <= 90){
+                //     alert( " you are aligible")
+                //  }
+                //  else{
+                //     alert("Sorry you are not aligible For this course")
+                //  }
+
+
+                // practice for percentage 
+
+            // let num=prompt("ente number")
+            // if ( num / 5 * 100 ){
+            //     alert ( " your percentage is " + num )
+            // }
+            
+            
+                // Checking for weekend or weekday program
+
+               //   let weekday = prompt(" enter day name");
+               //   if ( weekday === "monday" || weekday === "tuesday" || weekday === "wednesday" || weekday === "thruesday" || weekday === "friday"){                   alert("weekday " )
+               //   }
+               //   else if (weekday == "saturday" || weekday == "sunday")
+               //      {
+               //      alert("weekend days")
+               //   }
+               //   else{
+               //      alert("Not in the days")
+               //   }
+
+
+                // program for Night and day time 
+
+// alert("Please Enter Time Between 1 to 24 hour")
+//                 let time = prompt("Enter time ");
+//                 if ( time >= 1 && time <= 12)
+//                     {
+//                         alert ( time + "am Day time ")
+//                     }
+//                 else if ( time >= 13 && time <= 24){
+//                     alert(time + "pm Night time")
+//                 }
+//                 else{
+//                     alert("Out of range ");
+//                 }
+             
+
+// Program for login access 
+
+// let login= prompt("Enter Username ");
+// let paswoord= prompt("Enter Your paswoord");
+// if ( login == "Afridi" && paswoord == 123){
+//     alert("login successfuly ")
+// }
+// else{
+//     alert("Record Not foundeds")
+// }
